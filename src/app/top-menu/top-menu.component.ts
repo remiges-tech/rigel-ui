e8b4bee2ec976d79c8234f233032b148bdc8f1ab
sufiyaans <sufiyaan.shaik@merce.co>
2024-01-08 11:08:45 +0530

    i18n completed with language dropdown

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID, Inject } from '@angular/core';
 
+
+interface Locale {
+  localeCode: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-top-menu',
@@ -8,7 +13,24 @@ import { Component } from '@angular/core';
 })
 export class TopMenuComponent {
 
- 
-  constructor(){}
+  constructor(@Inject(LOCALE_ID) public locale: string) {}
+
+  
+  selectedLocale: string = this.locale;
+
+ // array of locales
+  locales: Locale[] = [
+    { localeCode: "en-US", label: "English" },
+    { localeCode: "hi", label: "Hindi" },
+    { localeCode: "gu", label: "Gujrati" },
+    { localeCode: "mr", label: "Marathi" },
+    { localeCode: "ar", label: "Arabic" },
+  ];
+
+  navigateToLocale(localeCode: string): void {
+    this.selectedLocale = this.locale;
+    const url = `/${localeCode}`; 
+    window.location.href = url;  // Change the window location directly
+  }
 
 }
