5d3ac04cd21ef4d356cba27e1bed48c77580838a
sufiyaans <sufiyaan.shaik@merce.co>
2024-01-10 11:05:58 +0530

    cursorFocus bug fixed

--- src/app/components/field-detail/field-detail.component.ts
+++ src/app/components/field-detail/field-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject, ElementRef, Renderer2 } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { Field } from 'src/models/common-interfaces';
 import { checkValueType } from 'src/utils/customValidator';
@@ -16,6 +16,10 @@ export class FieldDetailComponent {
   @Output() UpdateValues = new EventEmitter<Field>();
   value!: string;
 
+  constructor(
+    private renderer: Renderer2,
+    private el: ElementRef) { }
+
   updateChanges(data: Field) {
     this.isEdit = false;
     let verification = checkValueType(data.type, this.value!, data.constraints);
@@ -26,4 +30,13 @@ export class FieldDetailComponent {
     data.value = this.value;
     this.UpdateValues.emit(data)
   }
+
+  cursorFocus(id: any) {
+    setTimeout(()=>{
+      const inputElement = this.el.nativeElement.querySelector('#'+id);
+        this.renderer.selectRootElement(inputElement).focus();
+    },50)
+  }
 }
+
+
